64ea6e4e42498ac2184c2a481a71a75b168f9161
EC2 Default User <ec2-user@ip-172-31-86-126.ec2.internal>
2022-01-14 18:54:08 +0000

    Failure

--- app/models/record.rb
+++ app/models/record.rb
@@ -11,4 +11,13 @@ class Record < ApplicationRecord
   end
 
   enum work_status: {waiting_work: 0, start_work: 1, finish_work: 2, confirmed_work: 3}
+  
+  #グラフに出力するために使用
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) } 
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) } 
+  scope :created_2days_ago, -> { where(created_at: 2.days.ago.all_day) } 
+  scope :created_3days_ago, -> { where(created_at: 3.days.ago.all_day) } 
+  scope :created_4days_ago, -> { where(created_at: 4.days.ago.all_day) } 
+  scope :created_5days_ago, -> { where(created_at: 5.days.ago.all_day) } 
+  scope :created_6days_ago, -> { where(created_at: 6.days.ago.all_day) } 
 end
