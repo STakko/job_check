9f48aa1d4a48ee79f8e423a5772f0dfd0a053fd0
STakko <palutena915@gmail.com>
2022-01-20 11:25:51 +0000

    ajax-admin

--- app/controllers/admin/admin_comments_controller.rb
+++ app/controllers/admin/admin_comments_controller.rb
@@ -1,17 +1,18 @@
 class Admin::AdminCommentsController < ApplicationController
   before_action :authenticate_admin!
-  
+
   def create
-    record = Record.find(params[:record_id])
-    admin_comment = AdminComment.new(admin_comment_params)
-    admin_comment.record_id = record.id
-    admin_comment.save
-    redirect_to admin_record_path(record)
+    @record = Record.find(params[:record_id])
+    @admin_comment = AdminComment.new(admin_comment_params)
+    @admin_comment.record_id = @record.id
+    @admin_comment.save
+    @admin_comment = AdminComment.new
   end
 
   def destroy
+    @record = Record.find(params[:record_id])
     AdminComment.find_by(id: params[:id], record_id: params[:record_id]).destroy
-    redirect_to admin_record_path(params[:record_id])
+    @admin_comment = AdminComment.new
   end
 
   private
@@ -19,5 +20,5 @@ class Admin::AdminCommentsController < ApplicationController
   def admin_comment_params
     params.require(:admin_comment).permit(:comment)
   end
-  
+
 end
--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,6 +1,6 @@
 class Admin::EventsController < ApplicationController
   before_action :authenticate_admin!
-  
+
   def index
     @events = Event.page(params[:page]).reverse_order.per(9)
   end
